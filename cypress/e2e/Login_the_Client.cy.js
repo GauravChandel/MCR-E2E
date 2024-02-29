e3d0674f63f7f0e0eff6b7fed19ee319bdf8ccb0
GauravChandel <gaurav7217444@gmail.com>
2024-02-29 18:17:52 +0530

    Save

--- cypress/e2e/Login_the_Client.cy.js
+++ cypress/e2e/Login_the_Client.cy.js
@@ -1,20 +1,25 @@
-/// <reference types = "cypress" />
+/// <reference types="cypress" />
 
 import { LoginClient } from "../Selectors/Client_Login_CSS"
-const Login = new LoginClient()
+const Login = new LoginClient()    // Created the object of Loginclient class
 
 
-import cred from "../fixtures/clientcred.json"
+import cred from "../fixtures/clientcred.json" // Import the JSON fixture directly
 
-describe('Login the client' , ()=>{
-    before(()=>{
+
+describe('Login the client', () => {
+    beforeEach(() => {
         Login.OpenTheURL()
     })
 
-    it('Login the client', ()=>{
-        Login.Email_n_Password(cred.Client.Email, cred.Client.Password)
+    it('Login with valid credentials', () => {
+        cy.viewport(1920, 1080)
+        Login.Email_n_Password(cred[0].Client.Email, cred[0].Client.Password) // Access valid credentials from index 0
+        cy.contains('Dashboard(This is only design for now)').should('be.visible').and('have.class', 'pb-2 border-b border-gray-200 h3')
     })
 
-
-
+    it('Login with invalid credentials', () => {
+        cy.viewport(1920, 1080)
+        Login.Email_n_Password(cred[1].Invalid.Email, cred[1].Invalid.Password) // Access invalid credentials from index 1
+    })
 })
